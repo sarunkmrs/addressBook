a68e3f72a4402cd51ba420662e9c43a6f9d9c31e
Sarunkumar KG <sarunkmrs@gmail.com>
2014-12-24 15:34:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+      <meta name="description" content="Address Reference Book" />
+      <meta name="keyword" content="Address Reference Book" />
+      <meta name="author" content="SAR" />
+      <!-- Mobile Specific Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      
+      <!-- Libs CSS -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/font-awesome.min.css" rel="stylesheet">
+		
+		<!-- On Scroll Animations -->
+		<link href="css/animate.css" rel="stylesheet">
+		
+		<!-- Template CSS -->
+		<link href="css/style.css" rel="stylesheet">  
+		
+		<!-- Responsive CSS -->
+		<link href="css/responsive.css" rel="stylesheet"> 
+								
+		<!-- Favicons -->	
+		<link rel="shortcut icon" href="img/icons/favicon.ico">
+			
+		<!-- Google Fonts -->	
+		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" />
+      
+      <!-- 
+         HTML shim and Response.js files are local server. IE8 support of HTML5 elements and media queries
+      -->
+      <!--[if lt IE 9]>
+         <script src="js/html5shiv.js"></script>
+         <script src="js/respond.min.js"></script>
+      <![endif]-->
+      
+      <title>Address Reference Book</title>
+   </head>
+   <body>
+      <?php
+      // put your code here
+      ?>
+   </body>
+</html>
